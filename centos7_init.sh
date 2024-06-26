8dc02d77fe250ea5cc6dfe226cd77fc16f122bab
Hugh <Dang_Star@live.com>
2024-06-26 09:08:13 +0800

    Update centos7_init.sh

--- centos7_init.sh
+++ centos7_init.sh
@@ -64,7 +64,7 @@ cat > /etc/docker/daemon.json <<EOF
     "max-size": "10m",
     "max-file": "10"
   },
-  "registry-mirrors": ["https://docker.m.daocloud.io" ,"https://vh3bm52y.mirror.aliyuncs.com"]
+  "registry-mirrors": [ "https://registry.cn-hangzhou.aliyuncs.com" , "https://docker.m.daocloud.io"]
 }
 EOF
 echo "配置 Docker 镜像加速器完成"
