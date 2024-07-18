05122a63723c9c9e015d82f4da32374c0ed15208
Hugh <Dang_Star@live.com>
2024-07-18 08:39:04 +0800

    Update centos7_init.sh

--- centos7_init.sh
+++ centos7_init.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# 错误处理
+set -e
 
 # 函数：检查命令执行结果
 check_command() {
@@ -10,15 +12,26 @@ check_command() {
     fi
 }
 
-# 错误处理
-set -e
+# 函数：显示进度信息
+function show_progress() {
+    echo -e "\n[\033[1;32m+\033[0m] $1"
+}
+
+# 函数：显示错误信息并退出
+function show_error() {
+    echo -e "\n[\033[1;31m!\033[0m] 错误: $1"
+    exit 1
+}
+
+sudo yum update -y  || show_error "无法更新yum依赖"
 
 # 安装 Chrony
 if ! command -v chronyc &> /dev/null; then
-    echo "Chrony未安装，开始安装..."
-    yum install chrony -y
-    check_command "安装 Chrony"
+    yum install chrony -y || show_error "安装chrony"
+else
+    show_progress "Chrony 已经安装"
 fi
+
 
 # 设置时区
 ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
@@ -44,9 +57,8 @@ echo "重启 Chrony 服务完成"
 
 # 安装 Docker
 echo "安装docker所需依赖.."
-sudo yum update -y && \
-yum remove docker docker-common docker-selinux docker-engine -y && \
-yum install -y yum-utils device-mapper-persistent-data lvm2 && \
+yum remove docker docker-common docker-selinux docker-engine -y 
+yum install -y yum-utils device-mapper-persistent-data lvm2 
 yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo 
 
 echo "开始安装 Docker..."
@@ -64,7 +76,7 @@ cat > /etc/docker/daemon.json <<EOF
     "max-size": "10m",
     "max-file": "10"
   },
-  "registry-mirrors": ["https://cf-workers-docker-io-7cl.pages.dev", "https://registry.cn-hangzhou.aliyuncs.com" , "https://docker.m.daocloud.io"]
+  "registry-mirrors": ["https://cf-workers-docker-io-7cl.pages.dev", "https://registry.cn-hangzhou.aliyuncs.com" ]
 }
 EOF
 echo "配置 Docker 镜像加速器完成"
@@ -76,5 +88,3 @@ echo "停止并禁用防火墙完成"
 # 重启 Docker 服务
 systemctl daemon-reload && systemctl restart docker
 echo "重启 Docker 服务完成"
-
-echo "脚本执行完毕"
