a69510148023c9a150ae51c690859aab9abadb93
Hugh <Dang_Star@live.com>
2024-07-24 10:38:46 +0800

    Update centos7_init.sh

--- centos7_init.sh
+++ centos7_init.sh
@@ -3,14 +3,14 @@
 set -e
 
 # 函数：检查命令执行结果
-check_command() {
-    if [ $? -eq 0 ]; then
-        echo -e "\e[32m$1 完成\e[0m"  # 成功状态为绿色
-    else
-        echo -e "\e[31m$1 失败\e[0m"  # 失败状态为红色
-        exit 1
-    fi
-}
+# check_command() {
+#     if [ $? -eq 0 ]; then
+#         echo -e "\e[32m$1 完成\e[0m"  # 成功状态为绿色
+#     else
+#         echo -e "\e[31m$1 失败\e[0m"  # 失败状态为红色
+#         exit 1
+#     fi
+# }
 
 # 函数：显示进度信息
 function show_progress() {
@@ -41,14 +41,14 @@ echo "时区设置完成"
 # 启动和启用 Chrony 服务
 systemctl start chronyd.service
 systemctl enable chronyd.service
-echo "启动和启用 Chrony 服务完成"
+show_progress "启动和启用 Chrony 服务完成"
 
 # 修改 Chrony 配置文件
 if grep -q "server ntp.aliyun.com" /etc/chrony.conf; then
-    echo "已配置ntp.aliyun.com服务器"
+    show_progress "已配置ntp.aliyun.com服务器"
 else
     sed -i.bak -e '3,6 s/^/#/' -e '6a server ntp.aliyun.com minpoll 4 maxpoll 10 iburst' /etc/chrony.conf
-    check_command "修改 Chrony 配置文件"
+    show_progress "修改 Chrony 配置文件"
 fi
 # 重启 Chrony 服务并检查时间同步源
 systemctl restart chronyd.service || show_error "无法重启 Chrony 服务。"
@@ -57,14 +57,15 @@ systemctl restart chronyd.service || show_error "无法重启 Chrony 服务。"
 # 安装 Docker
 show_progress "移除旧 Docker..."
 yum remove docker docker-common docker-selinux docker-engine -y 
-echo "安装docker所需依赖.."
+show_progress "安装docker所需依赖.."
 yum install -y yum-utils device-mapper-persistent-data lvm2 
+show_progress "设置docker阿里镜像源.."
 yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo 
 
 show_progress "开始安装 Docker..."
 sudo yum install docker-ce -y 
 systemctl restart docker && systemctl enable docker
-check_command "重启Docker并设置开机启动"
+show_progress "重启Docker并设置开机启动"
 
 # 配置 Docker 镜像加速器
 cat > /etc/docker/daemon.json <<EOF
@@ -81,8 +82,8 @@ show_progress "配置 Docker 镜像加速器完成"
 
 # 停止并禁用防火墙
 systemctl stop firewalld && systemctl disable firewalld
-echo "停止并禁用防火墙完成"
+show_progress "停止并禁用防火墙完成"
 
 # 重启 Docker 服务
 systemctl daemon-reload && systemctl restart docker
-echo "重启 Docker 服务完成"
+show_progress "重启 Docker 服务完成"
