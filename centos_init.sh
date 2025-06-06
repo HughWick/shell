dbc45538a22117c36633f97d40169f9d0f01a6a4
Hugh <Dang_Star@live.com>
2025-06-06 14:39:25 +0800

    Update centos_init.sh

--- centos_init.sh
+++ centos_init.sh
@@ -259,7 +259,7 @@ cat > /etc/docker/daemon.json <<EOF
     "max-size": "10m",
     "max-file": "10"
   },
-  "registry-mirrors": [ "https://hub.littlediary.cn", "https://docker.hlmirror.com","https://hub.dockerx.org","https://cf-workers-docker-io-7cl.pages.dev"]
+  "registry-mirrors": [ "https://docker.1ms.run", "https://docker.hlmirror.com","https://hub.dockerx.org","https://cf-workers-docker-io-7cl.pages.dev"]
 }
 EOF
 show_progress "配置 Docker 镜像加速器完成"
